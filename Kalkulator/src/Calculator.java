44eddb87319a3b8242a8605d1cf987d0e3adcdff
KordianExigio <150010245+KordianExigio@users.noreply.github.com>
2024-09-19 20:13:31 +0200

    asdfasdf
    asdfasdfasdf

---
+++ Kalkulator/src/Calculator.java
@@ -0,0 +1,178 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener {
+
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
+
+    double num1 = 0;
+    double num2 = 0;
+    double wynik = 0;
+    char operator;
+
+    Calculator() {
+        // Tworzenie i ustawianie okna
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+        // Tworzenie i ustawianie pola tekstowego
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setEditable(false);
+        frame.add(textField);
+
+        // Tworzenie przycisków funkcyjnych
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+
+        // Dodawanie przycisków funkcyjnych do tablicy
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+
+        // Dodawanie ActionListener do przycisków funkcyjnych
+        for (JButton button : functionButtons) {
+            button.addActionListener(this);
+            button.setFocusable(false);
+        }
+
+        // Tworzenie i ustawianie przycisków numerycznych
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].setFont(new Font("Arial", Font.BOLD, 30));
+            numberButtons[i].setFocusable(false);
+            numberButtons[i].addActionListener(this);
+        }
+
+        // Ustawianie przycisków do panelu
+        delButton.setBounds(50, 430, 145, 50);
+        clrButton.setBounds(200, 430, 145, 50);
+
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(2, 2, 10, 10));
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(numberButtons[0]);
+        panel.add(decButton);
+        panel.add(equButton);
+        panel.add(divButton);
+        frame.add(panel);
+        frame.add(delButton);
+        frame.add(clrButton);
+
+
+        // Ustawienie widoczności okna
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Obsługa przycisków numerycznych
+        for (int i = 0; i < 10; i++) {
+            if (e.getSource() == numberButtons[i]) {
+                textField.setText(textField.getText().concat(String.valueOf(i)));
+            }
+        }
+
+
+        if (e.getSource() == decButton) {
+            textField.setText(textField.getText().concat("."));
+        }
+
+        if (e.getSource() == addButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '+';
+            textField.setText("");
+        }
+
+        if (e.getSource() == subButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '-';
+            textField.setText("");
+        }
+
+        if (e.getSource() == mulButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '*';
+            textField.setText("");
+        }
+
+        if (e.getSource() == divButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '/';
+            textField.setText("");
+        }
+
+        if (e.getSource() == equButton) {
+            num2 = Double.parseDouble(textField.getText());
+
+            switch (operator){
+                case '+':
+                    wynik = num1 + num2;
+                    break;
+
+                case '-':
+                    wynik = num1 - num2;
+                    break;
+
+                case '*':
+                    wynik = num1 * num2;
+                    break;
+
+                case '/':
+                    wynik = num1 / num2;
+                    break;
+            }
+
+            textField.setText(String.valueOf(wynik));
+            num1 = wynik;
+        }
+
+        if (e.getSource() == clrButton) {
+            textField.setText("");
+        }
+
+        if (e.getSource() == delButton) {
+            String string = textField.getText();
+            textField.setText("");
+            for (int i = 0; i < string.length() - 1; i++){
+                textField.setText(textField.getText()+string.charAt(i));
+            }
+        }
+    }
+}
